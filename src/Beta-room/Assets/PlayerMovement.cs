c05ca5f031d97a810e40363b23ec8073011f3eaa
Noah Kuite <nlkuite@gmail.com>
2020-10-17 13:58:48 -0400

    Adds test room with character controls

---
+++ src/Beta-room/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller; 
+
+    public float runSpeed = 40f;
+
+    float HorizontalMove = 0f; 
+    bool jump = false; 
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        if(Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+    }
+
+    void FixedUpdate () 
+    {
+        //move our character 
+        controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false; 
+
+    }
+}
