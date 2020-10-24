a2e604c16a161df32c21d33c7061db808763abfe
joshk <jkknibbe99@gmail.com>
2020-10-24 10:22:34 -0400

    Animation test

--- src/Beta-room/Assets/PlayerMovement.cs
+++ src/Beta-room/Assets/PlayerMovement.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller; 
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float HorizontalMove = 0f; 
     bool jump = false; 
+    int jumpInt = 0;
     
 
     // Update is called once per frame
     void Update()
     {
         HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed",Mathf.Abs(HorizontalMove));
         
         if(Input.GetButtonDown("Jump")){
             jump = true;
+            jumpInt = 1;
+            animator.SetFloat("Ups",Mathf.Abs(jumpInt));
         }
     }
 
@@ -27,6 +33,7 @@ public class PlayerMovement : MonoBehaviour
         //move our character 
         controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false; 
+        jumpInt = 0;
 
     }
 }
