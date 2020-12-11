94f4defa5995b91fdd299413e8e538b3e7ddc3a1
Noah Kuite <nlkuite@gmail.com>
2020-12-11 13:48:43 -0500

    Adds enemy spawner script

---
+++ src/Beta-room/Assets/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int maxSpawn;
+    float randX;
+    Vector2 whereToSpawn;
+    public float lowerX;
+    public float upperX;
+    int numSpawn;
+    // Start is called before the first frame update
+
+    //to spawn other enemy types, make a new spawer object, ad give it a different enemy 
+    void Start()
+    {
+        numSpawn = Random.Range(1,maxSpawn);
+        for(int i = 0; i < numSpawn; i++){
+            randX = Random.Range(lowerX, upperX);
+            whereToSpawn = new Vector2(randX, transform.position.y);
+            Instantiate(enemy, whereToSpawn, Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
