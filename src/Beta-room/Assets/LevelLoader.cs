21961155352e3b2ac8f9eb36e823ca4213a14b33
Noah Kuite <nlkuite@gmail.com>
2020-12-13 15:52:19 -0500

    Player can swap between every level

--- src/Beta-room/Assets/LevelLoader.cs
+++ src/Beta-room/Assets/LevelLoader.cs
@@ -13,9 +13,41 @@ public class LevelLoader : MonoBehaviour
             SceneManager.LoadScene(level);
         }
     }
+
+    public void changePosition()
+    {
+        int scene = SceneManager.GetActiveScene().buildIndex;
+        GameObject player = GameObject.Find("Player");
+
+        if (scene == 1)
+        {
+            player.transform.position = new Vector3(-13, -1, 0);
+        }
+        else if (scene == 2)
+        {
+            player.transform.position = new Vector3(-14, 1, 0);
+        }
+        else if (scene == 3)
+        {
+            player.transform.position = new Vector3(-5, -3, 0);
+        }
+        else if (scene == 4)
+        {
+            player.transform.position = new Vector3(-6, 3, 0);
+        }
+        else if (scene == 5)
+        {
+            player.transform.position = new Vector3(1, -2, 0);
+        }
+        else if (scene == 0)
+        {
+            player.transform.position = new Vector3(-13, -2, 0);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
+        changePosition();
         DontDestroyOnLoad(cam);
     }
 
